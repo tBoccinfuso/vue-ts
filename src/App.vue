726e9b935bc9e8c38da35e81ab77118500d0b629
Thomas Boccinfuso <tboccinfuso@gmail.com>
2018-11-15 22:27:00 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template lang='pug'>
+  #app
+    Header(creator='Thomas Boccinfuso')
+    #nav
+      router-link(to='/') Home
+      |  |
+      router-link(to='/about') About
+    router-view
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
+
+@Component({
+  components: {
+    Header,
+  },
+})
+
+export default class App extends Vue {}
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template lang='pug'>
+  nav.navbar(role='navigation' aria-label='main navigation')
+    p A Vue App
+      small  - By 
+        a(href="https://twitter.com/boccinfusoT" target="_blank" title="Creator of the App") {{ creator }}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {
+  @Prop() private creator!: string;
+}
+</script>
